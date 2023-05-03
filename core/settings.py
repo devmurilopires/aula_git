869f5ea06a27eb247c25708aaca57efff11ea74c
devmurilopires <murilopiresoficial@gmail.com>
2023-05-02 21:33:06 -0300

    algum commit

--- core/settings.py
+++ core/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'p'
 
 TIME_ZONE = 'America/Fortaleza'
 
